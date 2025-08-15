1853c18eaff67ab705d2b1283157875d969b2e69
sparklingp8 <mynewnokiap8@gmail.com>
2025-08-15 13:13:32 +0530

    home page UI added

--- podzen/home/urls.py
+++ podzen/home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view, show_link_view
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('show-link/<path:link>/', show_link_view, name='show_link'),
 ]
--- podzen/home/views.py
+++ podzen/home/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
+from urllib.parse import quote, unquote
 
 def home_view(request):
+    if request.method == 'POST':
+        youtube_link = request.POST.get('youtube_link')
+        if youtube_link:
+            return redirect('show_link', link=quote(youtube_link, safe=''))
     return render(request, 'home/home.html')
+
+def show_link_view(request, link):
+    return render(request, 'home/show_link.html', {'youtube_link': unquote(link)})
 
